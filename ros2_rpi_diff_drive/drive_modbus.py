e37116954ba33d29a3dd36a5ce8056a69f087516
Michael Jonathan <michaeljonathan664@gmail.com>
2024-05-10 12:57:44 +0700

    initiate

---
+++ ros2_rpi_diff_drive/drive_modbus.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ros2_rpi_diff_drive.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros2_rpi_diff_drive'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='blibli',
+    maintainer_email='michaeljonathan664@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'drive_modbus = ros2_rpi_diff_drive.drive_modbus:main'
+        ],
+    },
+)
