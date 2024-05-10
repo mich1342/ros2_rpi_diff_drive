7a2e07d2241c9dfabd2eab33a4695716b8bc5a1f
Michael Jonathan <michaeljonathan664@gmail.com>
2024-05-10 13:14:59 +0700

    add twist subscriber

--- ros2_rpi_diff_drive/drive_modbus.py
+++ ros2_rpi_diff_drive/drive_modbus.py
@@ -1,16 +1,32 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+
 from rclpy.clock import Clock
+import time
 
 from .ZltechDrive.ZltechDrive import *
 
 class DriveModbus(Node):
     def __init__(self):
         super().__init__('drive_modbs')
+
+        # Initialize Driver
         self.left_driver_ = ZltechDrive('/dev/ttyUSB0', 6)
-        self.left_driver_.set_velocity(10)
-        self.get_logger().info(f"Speed: {self.left_driver_.get_velocity()}")
+        self.left_driver_.set_velocity(0)
+        self.right_driver_ = ZltechDrive('/dev/ttyUSB0', 4)
+        self.right_driver_.set_velocity(0)
+
+        # cmd vel subscriber & callback
+        self.twist_sub_ = self.create_subscription(Twist, 'cmd_vel', self.twist_callback, 10)
+        self.target_vel_ = [0, 0]
+        self.prev_twist_t_ = time.time()
+
+    def twist_callback(self, msg):
+        self.target_vel_ = [msg.linear.x, msg.angular.z]
+        self.prev_twist_t_ = time.time()
+
 
 def main(args=None):
     rclpy.init(args=args)
